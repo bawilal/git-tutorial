7c01faebb46f8d3664db50a9b681669eb708886c
bawi <Bawilal2004@gmail.com>
2026-05-13 10:40:28 -0400

    Version 3

--- config.java
+++ config.java
@@ -1,7 +1,7 @@
 public class config {
     public static void main(String[] args) {
         System.out.println("config");
-        System.out.println("Version 2");
+        System.out.println("Version 3");
     }
     
 }
